e83df4b932c92d9a0b3c097118e405a78e5d6649
mohametalmeari <mohamet.almeari@gmail.com>
2023-05-03 02:47:59 +0300

    Create HTML, CSS, JS Files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
-import _ from 'lodash';
 import './style.css';
-function component() {
-  const element = document.createElement('div');
+import Dots from './dots.png';
+import Enter from './enter.png';
+import Refresh from './refresh.png';
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const toDoList = [
+  {
+    description: 'Reset my PC',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Call my family',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Call my fasadasmily',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Call my family',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Call my family',
+    completed: true,
+    index: 4,
+  },
+];
 
-  return element;
-}
-document.body.appendChild(component());
+const refreshIcon = document.getElementById('refresh-icon');
+refreshIcon.src = Refresh;
+const enterIcon = document.getElementById('enter-icon');
+enterIcon.src = Enter;
+
+const listContainer = document.getElementById('list-container');
+const ShowList = () => {
+  listContainer.innerHTML = '';
+  for (let i = 0; i < toDoList.length; i += 1) {
+    const listItem = document.createElement('li');
+    const checkIcon = document.createElement('input');
+    const span = document.createElement('span');
+    const dotsIcon = document.createElement('img');
+
+    listItem.id = `task${toDoList[i].index}`;
+    checkIcon.type = 'checkbox';
+    checkIcon.checked = toDoList[i].completed;
+    span.innerHTML = toDoList[i].description;
+    dotsIcon.src = Dots;
+
+    listItem.appendChild(checkIcon);
+    listItem.appendChild(span);
+    listItem.appendChild(dotsIcon);
+    listContainer.appendChild(listItem);
+  }
+};
+ShowList();
